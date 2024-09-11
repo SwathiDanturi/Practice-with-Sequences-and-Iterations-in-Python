b7708ae8d333ecf0a3e3d4f5a7f9be1d1a484b69
SwathiDanturi <Swathi.Danturi@unh.edu>
2024-09-11 00:40:36 -0400

    write test cases for div_nums()

--- test_div_nums.py
+++ test_div_nums.py
@@ -19,3 +19,14 @@ def test_nums_2_3_10_term_3():
     expected = [0, 1, 3]
     actual = div_nums(nums, term)
     assert actual == expected
+
+
+def test_nums_2_3_4_5_6_term_2():
+    """
+    Test for input [2, 3, 4, 5, 6] and 2.
+    """
+    nums = [2, 3, 4, 5, 6]
+    term = 2
+    expected = [1, 1, 2, 2, 3]
+    actual = div_nums(nums, term)
+    assert actual == expected
