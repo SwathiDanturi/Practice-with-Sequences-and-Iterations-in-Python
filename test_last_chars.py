ebdf539009f0f19970a7026724f18e12736aa57f
SwathiDanturi <Swathi.Danturi@unh.edu>
2024-09-11 14:59:53 -0400

    write test case 2 for last_chars()

--- test_last_chars.py
+++ test_last_chars.py
@@ -10,11 +10,21 @@ Collaborator(s): Samhitha
 from core import last_chars
 
 
-def test_last_chars_lastchars_od():
+def test_last_chars_lastchars_o_d():
     """
     Test for input ['hello', 'world']
     """
     word_list = ['hello', 'world']
     expected = ['o', 'd']
+    actual = last_chars(word_list)
+    assert actual == expected
+
+
+def test_last_chars_lastchars_d_g_e():
+    """
+    Test for input ['Integrated', 'Computing', 'Practice']
+    """
+    word_list = ['Integrated', 'Computing', 'Practice']
+    expected = ['d', 'g', 'e']
     actual = last_chars(word_list)
     assert actual == expected
