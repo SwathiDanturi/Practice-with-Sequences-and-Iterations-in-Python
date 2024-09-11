7a80421f2ba19f9ac622946f6aa26b2fd6e104d4
SwathiDanturi <Swathi.Danturi@unh.edu>
2024-09-11 14:56:31 -0400

    write test case 1 for last_chars()

--- test_last_chars.py
+++ test_last_chars.py
@@ -6,3 +6,15 @@ Date: 9/5/2024
 Developer: Swathi Danturi
 Collaborator(s): Samhitha
 """
+
+from core import last_chars
+
+
+def test_last_chars_lastchars_od():
+    """
+    Test for input ['hello', 'world']
+    """
+    word_list = ['hello', 'world']
+    expected = ['o', 'd']
+    actual = last_chars(word_list)
+    assert actual == expected
